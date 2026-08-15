5f4e37f483d529287757176a0615f1ecc4b4ef79
Tony Shangkuan @ Desktop <tonyshangkuan@gmail.com>
2026-08-15 17:28:42 +0800

    add message service

---
+++ messageservice/src/main/java/com/example/messageservice/MessageserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.messageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageserviceApplication.class, args);
+    }
+
+}
---
+++ messageservice/src/test/java/com/example/messageservice/MessageserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.messageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
