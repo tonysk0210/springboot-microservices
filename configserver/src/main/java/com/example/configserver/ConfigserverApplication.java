7f70dbe013ad97f21620984167ceaeaa4491a3d9
Tony Shangkuan @ Desktop <tonyshangkuan@gmail.com>
2026-08-30 14:31:51 +0800

    update

--- configserver/src/main/java/com/example/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/example/configserver/ConfigserverApplication.java
@@ -5,11 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
+@EnableConfigServer // 啟用 Config Server，讓應用程式提供集中式設定檔 API。
+/**
+ *   例如：
+ *
+ *   Account → http://configserver:8071/account/default
+ *   Loan    → http://configserver:8071/loan/default
+ *   Card    → http://configserver:8071/card/default
+ *
+ *   Config Server 會從 Git 或本機資料夾讀取：
+ *
+ *   configyml/account.yml
+ *   configyml/loan.yml
+ *   configyml/card.yml
+ */
 public class ConfigserverApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigserverApplication.class, args);
     }
-
 }
